37c473c459ed91cc98c080245da62d9306df7791
bcfolsom <bcfolsom@gmail.com>
2017-05-09 12:13:54 -0500

    updated allowed hosts

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'dev')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '45.32.200.184',
+]
 
 
 # Application definition
